a1c0d98588032db051cea6e4ab3017b3193c4fe6
Jeff Langevin <jeff.langevin@gmail.com>
2018-10-12 17:01:33 -0400

    Return to reactstrap after experiments with semantic ui

--- src/MortgagePayment.js
+++ src/MortgagePayment.js
@@ -1,129 +1,223 @@
 import React, { Component } from 'react';
-import { Dropdown, Form, Input, Popup } from 'semantic-ui-react';
+import SelectOptions from './SelectOptions';
+import { Button, Col, Container, Form, FormFeedback, FormGroup, Input, Label, Tooltip } from 'reactstrap';
+import { calculateLoanPayment, getValidDecimal } from './utils/loans.js';
 
 class MortgagePayment extends Component {
 
   state = {
-    rate:      5,
-    duration:  30,
-    principal: 450000,
-    payment:   ''
+    rate             : 5,
+    rateTipOpen      : false,
+    rateError        : null,
+    duration         : 30,
+    durationTipOpen  : false,
+    durationError    : null,
+    principal        : 450000,
+    principalTipOpen : false,
+    principalError   : null,
+    payment          : '',
   };
 
   componentDidMount() {
     this.calculatePayment();
   }
 
-  getValidNumber = (value, wholePlaces=2, decimalPlaces=2) => {
-    let regex = new RegExp("((?:\\d{0," + wholePlaces + "})(?:\\.\\d{0," + decimalPlaces + "}|\\d{0}))");
-    let match = value.match(regex);
-    return match.length ? match[0] : '';
-  };
-
-  handleAPRChange = (e, { name, value }) => {
-    let validVal = this.getValidNumber(value, 2, 2);
+  handleAPRChange = (event) => {
+    let { name, value } = event.currentTarget;
+    let validVal = getValidDecimal(value, 2, 2);
     if (value.length === 0 || validVal.length > 0) {
       this.setState({ [name]: validVal });
     }
   };
 
-  handlePrincipalChange = (e, { name, value }) => {
-    let validVal = this.getValidNumber(value, 10, 2);
+  handlePrincipalChange = (event) => {
+    let { name, value } = event.currentTarget;
+    let validVal = getValidDecimal(value, 10, 2);
     if (value.length === 0 || validVal.length > 0) {
       this.setState({ [name]: validVal });
     }
   };
 
-  handleFieldChange = (e, { name, value }) => {
+  handleFieldChange = (event) => {
+    let { name, value } = event.currentTarget;
     this.setState({ [name]: value });
   };
 
   calculatePayment = () => {
-    let { duration, principal } = this.state;
+    let { rate, duration, principal } = this.state;
 
-    // Convert the APR to a monthly rate by dividing by 100 and then by 12 months.
-    let monthlyRate = +this.state.rate / 100 / 12;
-    
-    // Multiply the mortgage term in years by 12 to get total monthly payments.
-    let months = +duration * 12;
-
-    // Raise ( 1 plus the monthly rate ) to the negative power of the number of monthly 
-    // payments. Subtract that result from 1.
-    let raise = 1 - Math.pow(monthlyRate + 1, -months);
-
-    // Divide the monthly rate by the result and multiply by the amount borrowed.
-    let payment = monthlyRate / raise * principal;
-
-    // Round payment
-    payment = Math.round(payment);
-
-    this.setState({ payment });
+    let stateUpdates = {
+      rateError      : ( typeof rate !== 'number' ) ? { invalid: true } : null,
+      principalError : ( typeof principal !== 'number' ) ? { invalid: true } : null,
+      payment        : ( typeof rate !== 'number' || typeof duration !== 'number' ) ?
+                       '' : 
+                       calculateLoanPayment(rate, duration, principal)
+    };
+ 
+    this.setState(stateUpdates);
   }
+
+  toggleTooltip = (tooltip) => {
+    let isOpen = this.state[tooltip];
+    this.setState({ [tooltip]: !isOpen });
+  }
+
+  durationOptions = [
+    { text: '5',  value: 5 },
+    { text: '10', value: 10 },
+    { text: '15', value: 15 },
+    { text: '20', value: 20 },
+    { text: '30', value: 30 },
+    { text: '35', value: 35 },
+    { text: '40', value: 40 }
+  ];
 
   render() {
 
-    const durationOptions = [
-      { text: '5',  value: 5 },
-      { text: '10', value: 10 },
-      { text: '15', value: 15 },
-      { text: '20', value: 20 },
-      { text: '30', value: 30 },
-      { text: '35', value: 35 },
-      { text: '40', value: 40 }
-    ];
+    const {
+      rateTipOpen,
+      rate,
+      rateError,
+      durationTipOpen,
+      duration,
+      principalTipOpen,
+      principal,
+      principalError,
+      payment
+    } = this.state;
 
     return (
-      <Form>
-        <Form.Field inline>
-          <Popup
-            trigger = {<label>Annual Interest Rate:</label>}
-            content = 'Annual interest rate (APR), not APY. Should be a percentage up to 99.99. Do not include %' />
-  
-          <Input
-            type     = 'text'
-            name     = "rate"
-            value    = { this.state.rate }
-            onChange = { this.handleAPRChange } />
-        </Form.Field>
+      <Container>
+        <h4>
+          Monthly Mortgage Payment Calculator
+        </h4>
 
-        <Form.Field inline>
-          <Popup
-            trigger = { <label>Loan Duration (Years):</label> }
-            content = 'How many years will you be paying this loan?' />
-          
-          <Dropdown
-            name     = "duration"
-            options  = { durationOptions }
-            value    = { this.state.duration }
-            onChange = { this.handleFieldChange }
-            selection /> 
-        </Form.Field>
-
-        <Form.Field inline>
-          <Popup
-            trigger = { <label>Loan Amount:</label> }
-            content = { `How much was the total amount borrowed? Do not include $ or commas` } />
-  
-          <Input
-            type     = 'text'
-            name     = "principal"
-            value    = { this.state.principal }
-            onChange = { this.handlePrincipalChange } />
-        </Form.Field>
+        <Form>
+          <FormGroup row>
+            <Label for='rate' id='rateLabel' xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+              Annual Interest Rate:
+            </Label>
+            
+            <Tooltip
+              placement = 'right'
+              isOpen    = { rateTipOpen }
+              target    = 'rateLabel'
+              toggle    = { () => this.toggleTooltip('rateTipOpen') }
+            >
+              Annual interest rate (APR), not APY. Should be a percentage up to 99.99. Do not include %
+            </Tooltip>
+            
+            <Col xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+              <Input
+                type     = 'text'
+                name     = 'rate'
+                id       = 'rate'
+                value    = { rate }
+                onChange = { this.handleAPRChange }
+                { ...rateError }
+              />
+              
+              {
+                rateError !== null &&
+                <FormFeedback>Rate is required</FormFeedback>
+              }  
+            </Col>
+          </FormGroup>
         
-        <Form.Button
-          content = "Calculate"
-          onClick = { this.calculatePayment }
-        />
+          <FormGroup row>
+            <Label for='duration' id='durationLabel' xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+              Loan Duration (Years):
+            </Label>
 
-        <div>
-          Monthly Payment: { `$${this.state.payment}` }*
-        </div>
+            <Tooltip
+              placement = 'right'
+              isOpen    = { durationTipOpen }
+              target    = 'durationLabel'
+              toggle    = { () => this.toggleTooltip('durationTipOpen') }
+            >
+              Enter the term of the loan in years.
+            </Tooltip>
 
-        <div>
-          * This is the principal payment only and does not include taxes, insurance, HOA dues, or other fees commonly associated with mortgages.
-        </div>
-      </Form>
+            <Col xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+              <Input
+                type     = 'select'
+                name     = 'duration'
+                id       = 'duration'
+                onChange = { this.handleFieldChange }
+                value    = { duration }
+              >
+                <SelectOptions  
+                  optionsArray = { this.durationOptions }
+                />
+              </Input>
+            </Col>
+          </FormGroup>
+
+          <FormGroup row>
+            <Label for='rate' id='principalLabel' xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+              Loan Amount:
+            </Label>
+
+            <Tooltip
+              placement = 'right'
+              isOpen    = { principalTipOpen }
+              target    = 'principalLabel'
+              toggle    = { () => this.toggleTooltip('principalTipOpen') }
+            >
+              How much was the total amount borrowed? Do not include $ or commas
+            </Tooltip>
+
+            <Col xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+              <Input
+                type     = 'text'
+                name     = 'principal'
+                id       = 'principal'
+                value    = { principal }
+                onChange = { this.handlePrincipalChange }
+                { ...principalError }
+              />
+            </Col>
+
+            {
+              principalError !== null &&
+              <FormFeedback>Principal is required</FormFeedback>
+            }
+          </FormGroup>
+        
+          <FormGroup row>
+            <Col xs={ 12 } md={ 8 } lg={ 6 }>
+              <Button color="success"
+                onClick = { this.calculatePayment }
+              >
+                Calculate
+              </Button>
+            </Col>
+          </FormGroup>
+          
+          <FormGroup row>
+            <Col xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+              Monthly Payment:
+            </Col>
+            
+            <Col xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
+              <b>
+                {
+                  rateError === null &&
+                  principalError === null &&
+                  `$${payment}*`
+                }
+              </b> 
+            </Col>
+          </FormGroup>
+
+          <FormGroup row>
+            <Col xs={ 12 } md={ 8 } lg={ 6 }>
+              <b>*</b> Monthly payment does not include taxes, insurance, 
+              HOA dues, or other fees commonly associated with mortgages.
+            </Col>
+          </FormGroup>
+        </Form>
+      </Container>
     );
   }
 };
