a5621af4d06fdd0958c79a5d77ea856e0fd4649c
Jeff Langevin <jeff.langevin@gmail.com>
2018-10-12 11:25:38 -0400

    Switch back to semUI after test; add decimal field functions

--- src/MortgagePayment.js
+++ src/MortgagePayment.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Message } from 'semantic-ui-react';
+import { Dropdown, Form, Input, Popup } from 'semantic-ui-react';
 
 class MortgagePayment extends Component {
 
@@ -14,12 +14,31 @@ class MortgagePayment extends Component {
     this.calculatePayment();
   }
 
+  getValidNumber = (value, wholePlaces=2, decimalPlaces=2) => {
+    let regex = new RegExp("((?:\\d{0," + wholePlaces + "})(?:\\.\\d{0," + decimalPlaces + "}|\\d{0}))");
+    let match = value.match(regex);
+    return match.length ? match[0] : '';
+  };
+
+  handleAPRChange = (e, { name, value }) => {
+    let validVal = this.getValidNumber(value, 2, 2);
+    if (value.length === 0 || validVal.length > 0) {
+      this.setState({ [name]: validVal });
+    }
+  };
+
+  handlePrincipalChange = (e, { name, value }) => {
+    let validVal = this.getValidNumber(value, 10, 2);
+    if (value.length === 0 || validVal.length > 0) {
+      this.setState({ [name]: validVal });
+    }
+  };
+
   handleFieldChange = (e, { name, value }) => {
     this.setState({ [name]: value });
   };
 
   calculatePayment = () => {
-
     let { duration, principal } = this.state;
 
     // Convert the APR to a monthly rate by dividing by 100 and then by 12 months.
@@ -39,53 +58,71 @@ class MortgagePayment extends Component {
     payment = Math.round(payment);
 
     this.setState({ payment });
-
   }
 
   render() {
 
+    const durationOptions = [
+      { text: '5',  value: 5 },
+      { text: '10', value: 10 },
+      { text: '15', value: 15 },
+      { text: '20', value: 20 },
+      { text: '30', value: 30 },
+      { text: '35', value: 35 },
+      { text: '40', value: 40 }
+    ];
+
     return (
       <Form>
+        <Form.Field inline>
+          <Popup
+            trigger = {<label>Annual Interest Rate:</label>}
+            content = 'Annual interest rate (APR), not APY. Should be a percentage up to 99.99. Do not include %' />
+  
+          <Input
+            type     = 'text'
+            name     = "rate"
+            value    = { this.state.rate }
+            onChange = { this.handleAPRChange } />
+        </Form.Field>
 
-        <Form.Input
-          inline
-          label="Annual Interest Rate:"
-          name="rate"
-          value={ this.state.rate }
-          onChange={ this.handleFieldChange }
-        />
+        <Form.Field inline>
+          <Popup
+            trigger = { <label>Loan Duration (Years):</label> }
+            content = 'How many years will you be paying this loan?' />
+          
+          <Dropdown
+            name     = "duration"
+            options  = { durationOptions }
+            value    = { this.state.duration }
+            onChange = { this.handleFieldChange }
+            selection /> 
+        </Form.Field>
+
+        <Form.Field inline>
+          <Popup
+            trigger = { <label>Loan Amount:</label> }
+            content = { `How much was the total amount borrowed? Do not include $ or commas` } />
+  
+          <Input
+            type     = 'text'
+            name     = "principal"
+            value    = { this.state.principal }
+            onChange = { this.handlePrincipalChange } />
+        </Form.Field>
         
-        <Form.Input 
-          inline
-          label="Loan Duration (Years):"
-          name="duration"
-          value={ this.state.duration }
-          onChange={ this.handleFieldChange }
-          error={ false }
-        />
-        <Message
-          error
-          header='Action Forbidden'
-          content='You can only sign up for an account once with a given e-mail address.'
-        />
-
-        <Form.Input
-          inline
-          label="Loan Amount:"
-          name="principal"
-          value={ this.state.principal }
-          onChange={ this.handleFieldChange }
-        />
-
         <Form.Button
-          content="Calculate"
-          onClick={ this.calculatePayment }
+          content = "Calculate"
+          onClick = { this.calculatePayment }
         />
 
-        
-        Monthly Payment: { `$${this.state.payment}` }*
+        <div>
+          Monthly Payment: { `$${this.state.payment}` }*
+        </div>
 
-        * This is the principal payment only and does not include taxes, insurance, HOA dues, or other fees commonly associated with mortgages.
+        <div>
+          * This is the principal payment only and does not include taxes, insurance, HOA dues, or other fees commonly associated with mortgages.
+        </div>
       </Form>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-// import 'semantic-ui-css/semantic.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'semantic-ui-css/semantic.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
