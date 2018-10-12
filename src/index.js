9d11a5238913751255dcd34ac6f7d0744eb86e17
Jeff Langevin <jeff.langevin@gmail.com>
2018-10-12 11:27:33 -0400

    Remove reactstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './App';
 
