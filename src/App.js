e4158650258121fc34ec6076827e98e66075d6d2
Jeff Langevin <jeff.langevin@gmail.com>
2018-10-17 09:01:04 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
-import MortgagePayment from './MortgagePayment';
+import { Container, Nav, Navbar, NavItem } from 'reactstrap';
+
+import Home from './components/Home';
+import MonthlyLoanPayment from './components/calculators/MonthlyLoanPayment';
+import DebtRepayment from './components/calculators/DebtRepayment';
+import MortgageAffordability from './components/calculators/MortgageAffordability';
 import './App.css';
 
 
@@ -8,14 +13,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav>
-          <Link to="/MortgagePayment">Mortgage Payment</Link>
-        </nav>
-        <div>
+        <Navbar color="light" dark>
+          <Nav navbar>
+            <NavItem>
+              <Link to="/">Home</Link>
+            </NavItem>
+          </Nav>
+        </Navbar>
+
+        <Container>
           <Route
-            path      = "/MortgagePayment"
-            component = { MortgagePayment } />
-        </div>
+            path      = "/MonthlyLoanPayment"
+            component = { MonthlyLoanPayment } />
+
+          <Route
+            path      = "/DebtRepayment"
+            component = { DebtRepayment } />
+
+          <Route
+            path      = "/MortgageAffordability"
+            component = { MortgageAffordability } />
+
+          <Route
+            path      = "/"
+            component = { Home }
+            exact />
+        </Container>
       </div>
     );
   }
